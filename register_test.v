0acc9778ff9f873c3f0d172e57b38d1c24c136bd
unknown <contact.f37@gmail.com>
2016-01-16 19:18:02 +0100

    cleaning some old comment

--- register_test.v
+++ register_test.v
@@ -34,41 +34,9 @@ reg clk;
 	wire [31:0]r_data1;
 	wire [31:0]r_data2;
 	reg clr;
-	/*
-	wire [4:0]r_reg1;
-	wire [4:0]r_reg2;
-	wire [4:0]w_reg_addr;
-	wire [31:0]w_data;
-	wire reg_w;
-	wire [31:0]r_data1;
-	wire [31:0]r_data2;
-	wire clr;*/
-	/*assign r_reg1 = 0;
-	assign r_reg2 = 1;
-	assign w_reg_addr = 0;
-	assign w_data = 0;
-	assign r_data1 = 0;
-	assign r_data2 = 0;
-	assign reg_w = 0;
-	assign clr = 0;*/
 
-/*
-always begin
-	  clk <= 1;
-	  #1000;
-	  clk <= 0;
-	  #1000;
-	  
-	  r_reg1 <= 0;
-	  r_reg2 <= 1;
-	  w_reg_addr <= 0;
-	  w_data <= 0;
-	  r_data1 <= 0;
-	  r_data2 <= 0;
-	  reg_w <= 0;
-	  clr <= 0;
-end
-*/
+
+
 integer i;
 initial begin 
 	clk = 1; 
@@ -77,8 +45,6 @@ initial begin
 	r_reg2 <= 0;
 	w_reg_addr <= 0;
 	w_data <= 0;
-	//r_data1 <= 0;
-	//r_data2 <= 0;
 	reg_w <= 0;
 	clr <= 0;
 end
@@ -106,47 +72,8 @@ always begin
 	i = i+1;
 end
 
-	//#(500) clk = ~clk; 
-	/*clk <= 1;
-	reg_w <= 1;
-	w_data <= 1256;
-	w_reg_addr <= 3;
-	#200;
-	reg_w <= 0;
-	w_data <= 0;
-	w_reg_addr <= 0;
-	#200
-	r_reg1 <= 3;
 
-*/
-//clk <= 1;
 	  
-	  /*  force reg_w = 1;
-		 force w_data = 1256;
-		 force w_reg_addr = 3;
-	  #200;
-		 force reg_w = 0;
-		 force w_data = 0;
-		 force w_reg_addr = 0;
-	  #200;
-	    force r_reg1 = 3;*/
-	 /* #200;
-		 force r_reg2 = 3;
-	  #200;
-		force reg_w = 1;
-		force w_data = 1111;
-		force w_reg_addr = 5;
-	  #200;
-		force reg_w = 1;
-		force w_data = 5555;
-		force w_reg_addr = 10;
-	  #200;
-		force reg_w = 0;
-		force w_data = 0;
-		force w_reg_addr = 0;
-		force r_reg1 = 10;
-		force r_reg2 = 5; */
-		
 //INPUT
 // 1bit  clk = clock 										
 // 5bit  r_reg => read in register1
